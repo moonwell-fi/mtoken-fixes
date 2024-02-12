465083dc66fbcc33fcd622f6fc577213079700c7
bsolid <brent@soliditylabs.io>
2024-02-12 20:32:14 +0800

    chore: deleted an unused interface

--- src/Interfaces/IMGlimmer.sol
+++
@@ -1,7 +0,0 @@
-pragma solidity 0.8.19;
-
-/// @title MGlimmer token interface
-interface IMGlimmer {
-    /// @notice balance of a given address
-    function balanceOf(address) external returns (uint256);
-}
