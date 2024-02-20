fc9cdf322d9616d959f8003e6cac2b75bd55b338
Elliot <elliotfriedman3@gmail.com>
2024-02-19 16:44:55 -0800

    sweepAll function
    Signed-off-by: Elliot <elliotfriedman3@gmail.com>

--- src/MErc20DelegateMadFixer.sol
+++ src/MErc20DelegateMadFixer.sol
@@ -4,33 +4,13 @@ import "./MErc20Delegate.sol";
 
 /// @title MErc20DelegateMadFixer contract
 contract MErc20DelegateMadFixer is MErc20Delegate {
-    /// @notice user fixed event (user, liquidator, amount)
-    event TokensSwept(address, uint256);
-
     /// @notice sweep underlying tokens
     /// @param sweeper address of the sweeper
     function sweepAll(address sweeper) external {
         /// @dev checks
         require(msg.sender == admin, "only admin may sweep all");
-
-        uint256 amount = EIP20Interface(underlying).balanceOf(address(this));
-        EIP20NonStandardInterface token = EIP20NonStandardInterface(underlying);
-        require(amount > 0, "balance must be greater than zero");
-
+        EIP20Interface token = EIP20Interface(underlying);
         /// @dev take it, take it all
-        token.transfer(sweeper, amount);
-    }
-
-    /// @notice balance
-    /// @return balance held by the contract
-    function balance() public view returns (uint256) {
-        return EIP20Interface(underlying).balanceOf(address(this));
-    }
-
-    /// @notice balance for a given account
-    /// @param account to get the balance of
-    /// @return balance held by the account
-    function balanceOf(address account) public view returns (uint256) {
-        return EIP20Interface(underlying).balanceOf(account);
+        token.transfer(sweeper, token.balanceOf(address(this)));
     }
 }
